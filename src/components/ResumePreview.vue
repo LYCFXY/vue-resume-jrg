b61e13a88b0b8dd288fe51c20322ab936594e110
wanghu <898639198@qq.com>
2017-11-23 22:59:52 +0800

    fist-add

---
+++ src/components/ResumePreview.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="resumePreview">
+        I am resumePreview
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        name: 'ResumePreview'
+    }
+</script>
+
+<style type="text/css">
+    #resumePreview{
+        background:#ffffff;
+        box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+    }
+</style>
